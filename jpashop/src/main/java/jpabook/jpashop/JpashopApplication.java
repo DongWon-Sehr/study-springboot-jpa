a45b2ef9daaf29e84f347496ff0460024b83c04b
DongWon Sehr <ddoro36@gmail.com>
2023-04-25 08:05:45 +0900

    remove test code

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-		LombokTest lombokTest = new LombokTest();
-		lombokTest.setData("hi, lombok!");
-		String data = lombokTest.getData();
-		System.out.println(" data: " + data);
-
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
