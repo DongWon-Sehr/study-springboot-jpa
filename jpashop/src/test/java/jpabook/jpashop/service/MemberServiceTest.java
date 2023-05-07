abc61acc66ccd8902b3771cc1981f258f98a8cb5
DongWon Sehr <ddoro36@gmail.com>
2023-05-08 08:10:00 +0900

    rollback false to check insert query

--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +22,7 @@ public class MemberServiceTest {
     @Autowired MemberRepository memberRepository;
     
     @Test
+    @Rollback(false)
     public void join() throws Exception {
         
         // given
