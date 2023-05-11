2c133cb46d1676a94be4bac248aa5cb0f9dfb08b
DongWon Sehr <ddoro36@gmail.com>
2023-05-12 06:51:11 +0900

    remove fail package

--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -2,7 +2,6 @@ package jpabook.jpashop.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import javax.persistence.EntityManager;
 
