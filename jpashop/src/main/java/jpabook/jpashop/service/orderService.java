1d33c8df8a866dde0ac098d94335d25ba62e57a3
DongWon Sehr <ddoro36@gmail.com>
2023-06-08 07:58:59 +0900

    + orderService

---
+++ jpashop/src/main/java/jpabook/jpashop/service/orderService.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.memberRepository;
+import jpabook.jpashop.repository.orderRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class orderService {
+    
+    private final orderRepository orderRepository;
+    private final memberRepository memberRepository;
+    private final ItemRepository itemRepository;
+    
+    // order
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        
+        // find entity
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // create delivery info
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress()); // use member address for implement, but need another address for production
+
+        // create orderItem
+        OrderItem orderitem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // create order
+        Order order = Order.creatOrder(member, delivery, orderitem);
+
+        // save order
+        orderRepository.save(order); // due to cascade option @ Order Class, persist Order, OrderItem, Delivery entities at once
+
+        return order.getId();
+    }
+
+    // cancel
+
+    // search
+}
