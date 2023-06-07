1b7031e136bf458362c3215b929833a3398cc200
DongWon Sehr <ddoro36@gmail.com>
2023-06-08 07:13:59 +0900

    + itemService test code

---
+++ jpashop/src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+public class ItemServiceTest {
+    
+    @Autowired ItemService itemService;
+    @Autowired ItemRepository itemRepository;
+    @Autowired EntityManager em;
+
+    @Test
+    public void save() throws Exception {
+        // given
+        Item book = new Book();
+        book.setName("The Little Prince");
+        book.setPrice(15000);
+        book.setStockQuantity(10);
+
+        // when
+        itemService.saveItem(book);
+
+        // then
+        assertEquals(book, itemService.findOne(book.getId()));
+    }
+}
