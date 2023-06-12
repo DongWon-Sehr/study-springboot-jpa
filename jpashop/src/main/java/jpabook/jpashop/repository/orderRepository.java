21511bd5eba49e0e17bbc2fd969867d48f2fcb78
DongWon Sehr <ddoro36@gmail.com>
2023-06-12 21:57:52 +0900

    implement orderSearch Repo

--- jpashop/src/main/java/jpabook/jpashop/repository/orderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/orderRepository.java
@@ -1,8 +1,16 @@
 package jpabook.jpashop.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -23,8 +31,26 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    // public List<Order> findAll(OrderSearch ordersearch) {
-    //     return em.createQuery("select o from  Order o", Order.class)
-    //             .getResultList();
-    // }
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        if (orderSearch.getMemberName() != null) {
+            Predicate name = cb.equal(o.get("name"), orderSearch.getMemberName());
+            criteria.add(name);
+        }
+
+        cq.where(criteria.toArray(new Predicate[criteria.size()]));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+        }
 }
