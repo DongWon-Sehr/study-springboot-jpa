4f3c2e0e53a34af3afe2a39b225a4a7f272af7d9
DongWon Sehr <ddoro36@gmail.com>
2023-06-19 07:49:51 +0900

    + update test

--- jpashop/src/test/java/jpabook/jpashop/service/ItemServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import javax.persistence.EntityManager;
 
@@ -8,11 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import jpabook.jpashop.controller.BookForm;
 import jpabook.jpashop.domain.item.Book;
-import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 
 @ExtendWith(SpringExtension.class)
@@ -27,15 +29,78 @@ public class ItemServiceTest {
     @Test
     public void saveTest() throws Exception {
         // given
-        Item book = new Book();
+        Book book = new Book();
         book.setName("The Little Prince");
         book.setPrice(15000);
-        book.setStockQuantity(10);
+        book.setStockQuantity(15);
+        book.setAuthor("Saint-Exupéry");
+        book.setIsbn("1234");
 
         // when
         itemService.saveItem(book);
 
         // then
         assertEquals(book, itemService.findOne(book.getId()));
+    }
+
+    @Test
+    public void updateTestWithUpdateItem() throws Exception {
+        // given
+        Book book = new Book();
+        book.setName("The Little Prince");
+        book.setPrice(15000);
+        book.setStockQuantity(15);
+        book.setAuthor("Saint-Exupéry");
+        book.setIsbn("1234");
+        itemService.saveItem(book);
+
+        String updateName = "The Middle Prince";
+        int updatePrice = 10000;
+        int updateStockQuantity = 10;
+
+        BookForm bookForm = new BookForm();
+        bookForm.setId(book.getId());
+        bookForm.setName(updateName);
+        bookForm.setPrice(updatePrice);
+        bookForm.setStockQuantity(updateStockQuantity);
+
+        UpdateItemDto updateBookDto = new UpdateItemDto(book.getId(), bookForm);
+
+        // when
+        itemService.updateItem(updateBookDto);
+        
+        // then
+        assertEquals(updateName, itemService.findOne(book.getId()).getName());
+        assertEquals(updatePrice, itemService.findOne(book.getId()).getPrice());
+        assertEquals(updateStockQuantity, itemService.findOne(book.getId()).getStockQuantity());
+    }
+
+    @Test
+    public void updateTestWithSaveItem() throws Exception {
+        // given
+        Book book = new Book();
+        book.setName("The Little Prince");
+        book.setPrice(15000);
+        book.setStockQuantity(15);
+        book.setAuthor("Saint-Exupéry");
+        book.setIsbn("1234");
+        itemService.saveItem(book);
+
+        String updateName = "The Middle Prince";
+        int updatePrice = 10000;
+        int updateStockQuantity = 10;
+
+        Book updateBook = new Book();
+        updateBook.setId(book.getId());
+        updateBook.setName(updateName);
+        updateBook.setPrice(updatePrice);
+        updateBook.setStockQuantity(updateStockQuantity);
+        updateBook.setAuthor(book.getAuthor());
+        updateBook.setIsbn(book.getIsbn());
+
+        // when
+        assertThrows(InvalidDataAccessApiUsageException.class, () -> itemService.saveItem(updateBook)); // throw exception
+        
+        // then
     }
 }
