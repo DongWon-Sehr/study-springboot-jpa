3315821eb6b30b31efde1f33cd7559fcaac6581f
DongWon Sehr <ddoro36@gmail.com>
2023-06-20 07:24:19 +0900

    + orderlist controller

--- jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -5,11 +5,17 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
@@ -42,8 +48,21 @@ public class OrderController {
     public String order(@RequestParam("memberId") Long memberId, 
                         @RequestParam("itemId") Long itemId, 
                         @RequestParam("count") int count) {
+        log.info("OrderController - POST order");
 
         Long orderId = orderService.order(memberId, itemId, count);
         return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        log.info("OrderController - GET orderList");
+        String json = objToJson(orderSearch);
+        log.info(json);
+        
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "/order/orderList";
     }
 }
