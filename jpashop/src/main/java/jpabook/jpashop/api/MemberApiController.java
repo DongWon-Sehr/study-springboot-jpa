1424253acc1f26980b4ea31be97ffecce6230ed4
DongWon Sehr <ddoro36@gmail.com>
2023-07-05 08:20:35 +0900

    update get method response dto spec - add count

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -7,9 +7,9 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,25 +40,25 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
-    @PatchMapping("/api/v2/members/{id}")
+    @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request) {
-        memberService.updateMember(id, request.getName());
-        Member findMember = memberService.findOne(id);
+        memberService.updateMember(id, request.getName()); // update logic
+        Member findMember = memberService.findOne(id); // seperate query logic to update logic
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
 
     @GetMapping("/api/v1/members")
     public List<Member> getMembersV1() {
-        return memberService.findMembers();
+        return memberService.findMembers(); // do not directly expose entity @ production service
     }
 
     @GetMapping("/api/v2/members")
     public Result getMembersV2() {
         List<Member> findMembers = memberService.findMembers();
         List<GetMemberDto> collect = findMembers.stream()
-                                        .map(m -> new GetMemberDto(m.getId(), m.getName()))
-                                        .collect(Collectors.toList());
-        return new Result(collect);
+            .map(m -> new GetMemberDto(m.getId(), m.getName()))
+            .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
     }
 
     @Data
@@ -93,6 +93,7 @@ public class MemberApiController {
     @Data
     @AllArgsConstructor
     static class Result<T> {
+        private int count;
         private T data;
     }
 
