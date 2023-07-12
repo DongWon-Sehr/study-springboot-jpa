ecdf914f0a1b66e1a68b8a3cacb7e9191d81d80d
DongWon Sehr <ddoro36@gmail.com>
2023-07-13 08:22:08 +0900

    create OrderQueryService for OSIV off

---
+++ jpashop/src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.service.query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderQueryService {
+    
+    private final OrderRepository orderRepository;
+
+    public List<OrderDto> getOrdersV3() {
+        return orderRepository.findAllByItem().stream()
+            .map(OrderDto::new)
+            .collect(Collectors.toList());
+    }
+}
